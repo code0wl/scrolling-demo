dda6d8eafad7437a68cfcbdc83bb7c67edc9be96
Oscar <oscar@bravepixel.nl>
2023-07-30 10:13:33 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScrollDemoComponent } from './scroll-demo/scroll-demo.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scroll-demo/scroll-demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-demo',
+  templateUrl: './scroll-demo.component.html',
+  styleUrls: ['./scroll-demo.component.scss']
+})
+export class ScrollDemoComponent {
+
+}
